f1ffa08d4d020772cbacc7082984d58c34e99878
Duy <dev.anhduy@gmail.com>
2020-08-26 17:57:39 +0700

    update

--- server/loaders/mongodb.js
+++ server/loaders/mongodb.js
@@ -4,8 +4,12 @@ const mongodb_config = require(__root + '/config/mongodb')
 
 module.exports = async () =>{
     try {
-        mongoose.connect(mongodb_config.url,mongodb_config.options );
-    }catch(err){
+        mongoose.connect(mongodb_config.url,mongodb_config.options,(err) => {
+            if(err) console.log(err);
+            else
+                console.log('Connect DB successfully !')
+        });
+    } catch(err){
         logger.error(err);
     }
     mongoose.Promise = global.Promise;
