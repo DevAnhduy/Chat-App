f7f745e312dbc1965fa7a8ba41ed8af00fe9c365
DevAnhduy <dev.anhduy@gmail.com>
2020-08-27 23:54:45 +0700

    add file query temp

---
+++ server/src/utils/file_query.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const File_Model = require('src/models/')
+class File_Query {
+    constructor(path_file_folder,receiver_type,query_options){
+        this.path_file_folder = path_file_folder,
+        this.receiver_type = receiver_type,
+        this.query_options = query_options
+    }
+
+    find(){
+        
+    }
+}
