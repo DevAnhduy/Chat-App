3822b2905abe03bd85327b6cb5f3b165a1ad1ea1
Duy <dev.anhduy@gmail.com>
2020-08-31 18:37:14 +0700

    Update check-auth.js

--- client/src/utils/check-auth.js
+++ client/src/utils/check-auth.js
@@ -1,21 +1,28 @@
 import Axios from 'axios';
 const { __server } = require('config/constant.json');
+const jwt_decoded = require('jwt-decode');
 
 const check_auth = async (callback) => {
     try {
-        Axios.get(`${process.env.REACT_APP_API_URL}/check-auth`, 
-            { 
-              headers: {
-                authorization : window.localStorage.token
-              } 
-            })
-            .then(response => {
-              console.log(response)
-                return callback(response.data);
-            })
-            .catch(error => {
-              console.log(error)
-            })
+        if(localStorage.token){
+          return callback(jwt_decoded(localStorage.token))
+        }
+        else {
+          return callback(false)
+        }
+        // Axios.get(`${process.env.REACT_APP_API_URL}/check-auth`, 
+        //     { 
+        //       headers: {
+        //         authorization : window.localStorage.token
+        //       } 
+        //     })
+        //     .then(response => {
+        //       console.log(response)
+        //         return callback(response.data);
+        //     })
+        //     .catch(error => {
+        //       console.log(error)
+        //     })
     }
     catch (err) {
         console.log(err)
