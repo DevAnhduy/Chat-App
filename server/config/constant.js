be2cf1f667329b67ce49374ee4baaeefe9f33ac1
Duy <dev.anhduy@gmail.com>
2020-08-31 18:37:18 +0700

    Update constant.js

--- server/config/constant.js
+++ server/config/constant.js
@@ -6,3 +6,5 @@ global['__port'] = server.port;
 global['__root'] = path.dirname(process.mainModule.filename).replace(/\\/g,"/");
 global['__images'] = path.join(__root, "images");
 global['__data'] = path.join(__root, "data");
+global['__messages'] = path.join(__data,'messages');
+global['__files'] = path.join(__data,'files');
