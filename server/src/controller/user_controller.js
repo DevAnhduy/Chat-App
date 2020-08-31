8266cad95ca2d3c6d041cd7722a736ce61be1afd
Duy <dev.anhduy@gmail.com>
2020-08-31 18:37:43 +0700

    Update user_controller.js

--- server/src/controller/user_controller.js
+++ server/src/controller/user_controller.js
@@ -5,44 +5,10 @@ const logger = require('src/utils/logger');
 const path = require('path');
 const factory = require('src/controller/handle_factory');
 const User_Model = require('src/models/user');
+const auth_controller = require('src/controller/auth_controller');
 
-exports.login = (req,res) => {
-    User_Model.findOne({ username: req.body.username })
-        .exec()
-        .then(user => {
-            if (user) {
-                bcrypt.compare(req.body.password, user.password, (err, result) => {
-                    if (err)
-                        return res.status(401).json({ message: 'Auth failed' });
-                    if (result) {
-                        const token = jwt.sign({
-                            username: user.username,
-                            user_id: user._id
-                        }, process.env.JWT_KEY )
-                        // res.cookie(
-                        //     'JWT',token,
-                        //     {   
-                        //         maxAge: 9999999,
-                        //         httpOnly: true
-                        //     }
-                        // )
-                        return res.status(200).json({token: token})
-                    }
-                    else {
-                        return res.status(401).json(false)
-                    }
-                })
-            }
-            else{
-                logger.error('User not found!')
-                return res.status(200).json({ login_success: false })
-            }   
-        })
-        .catch(err => {
-            logger.error(err);
-            res.status(500).json({ error: 'Something error. Please try again' })
-        })
-}
+exports.login = auth_controller.login;
+
 exports.get_all_users = factory.get_all(User_Model);
 
 exports.get_user = factory.get_one(User_Model);
