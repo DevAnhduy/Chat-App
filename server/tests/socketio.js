e33151bbde726e23ed2c7c4efab8372e480f34f5
DevAnhduy <dev.anhduy@gmail.com>
2020-09-12 20:09:28 +0700

    Update socketio.js

--- server/tests/socketio.js
+++ server/tests/socketio.js
@@ -1,90 +1,53 @@
 const request = require('request');
 const User_Model = require('src/models/user');
-const { ObjectID } = require('mongodb');
 const encode_jwt_token = require('src/utils/encode_jwt_token');
+const moment= require('moment');
 module.exports = (server) => {
     let user_connected = {};
     const io = require('socket.io')(server);
-    io.on('connection', (socket) => {
+    io.on('connection', async (socket) => {
         console.log('A user connected!');
         const jwt_token = socket.handshake.query.authorization;
-        const user = encode_jwt_token(jwt_token);
-        user_connected[user.user_id] = socket.id;
-        // socket.id = user.user_id;
-        // socket.conn.transport.sid = user.user_id;
-        // socket.conn = user.user_id;
-        // socket.client.id = user.user_id;
-        // socket.client.conn.id = user.user_id;
-        //console.log(socket.id);
-        // if (socket.id === '5eea1bc4c83863222c9d0e4f'){
-        //     socket.join('123123')
-        //     console.log(socket)
-        // }
-        // socket.join(user.user_id);
-        // if (user.user_id != '5eea1bc4c83863222c9d0e4f')
-        //     socket.join('5eea1bc4c83863222c9d0e4f');
-        // io.to('5eea1bc4c83863222c9d0e4f').emit('test',{data: 'Hello'})
-        //request.put(`${__host}:${__port}/users/${user.user_id}/${socket.id}`);
+        const decode_token = encode_jwt_token(jwt_token);
+        // const get_user_info = new Promise((resolve,reject) => {
+        //     User_Model.findById(decode_token.user_id)
+        //         .then(response => resolve(response))
+        //         .catch(error => reject(error))
+        //     // request.get({
+        //     //     url: `${__host}:${__port}/users/${decode_token.user_id}`,
+        //     //     headers: {
+        //     //         authorization: jwt_token
+        //     //     }
+        //     // },(err,response) => {
+        //     //     if(err) reject(err)
+        //     //     else resolve(JSON.parse(response.body).data)
+        //     // })
+        // })
+        const user = await User_Model.findById(decode_token.user_id) ;
+        user_connected[user._id] = socket.id;
         socket.on('/join-room', (data) => {
             socket.removeAllListeners('/send-message');
-            const token = socket.handshake.query.authorization;
             if(data.type === 'rooms')
                 socket.join(data.room_id);
 
             socket.on('/send-message', (msg) => {
-                if(user.username){
-                    if(data.type === 'rooms')
-                        io.to(data.room_id).emit('/send-message', { 
-                            sender: user.username, 
-                            sender_id: user.user_id,
-                            content: msg.content });
-                    else {
-                        io.to(user_connected[data.room_id]).emit('/send-message', 
-                        { 
-                          sender_id: user.user_id, 
-                          sender: user.username, 
-                          content: msg.content
-                        });
-                        io.to(user_connected[user.user_id]).emit('/send-message', 
-                        { 
-                          sender_id: user.user_id, 
-                          sender: user.username, 
-                          content: msg.content,
-                        });
+                const message_to_client = {
+                    sender_id,
+                    receiver_id: data.room_id,
+                    content: msg.content
+                }
+                if (data.type === 'rooms')
+                    io.to(data.room_id).emit('/send-message', message_to_client);
+                else {
+                    io.to(user_connected[data.room_id]).emit('/send-message',message_to_client);
+                    io.to(user_connected[user._id]).emit('/send-message', message_to_client);
+                }
+                request.post({
+                    url: `${__host}:${__port}/chat/${data.type}/${data.room_id}/messages`,
+                    headers: {
+                        authorization: jwt_token
                     }
-                    request.post({
-                        url : `${__host}:${__port}/chat/${data.type}/${data.room_id}/messages`,
-                        headers: {
-                            authorization: token
-                        }
-                    }).form({content: msg.content});
-                }
-            })
-        })
-        socket.on('/chat-with-user', (data) => {
-            socket.removeAllListeners('/send-messages/users');
-            socket.on('/send-messages/users', (msg) => {
-                if(user.username){
-                    console.log(user)
-                    User_Model.findById(ObjectID(body.receiver_id))
-                        .select('username socket_id')
-                        .exec()
-                        .then(receiver => {
-                            if (receiver){
-                                io.to(receiver.socket_id).emit('/send-messages/users',{
-                                    sender: user.username,
-                                    content: msg.content
-                                })
-                                request.post(`${_host}:${__port}/chat/users/${user.user_id}/messages`).form({
-                                    receiver_id: body.receiver_id,
-                                    content: msg.content
-                                })
-                            }
-                        })
-                        .catch(err => {
-                            console.log(err)
-                        })
-                }
+                }).form({content: msg.content});
             })
         })
     })
