660584eef449f48290872f50c55e8a55b5df9905
DevAnhduy <dev.anhduy@gmail.com>
2020-09-16 00:05:27 +0700

    Update index.js

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import {BrowserRouter as Router,Route,Switch } from 'react-router-dom'
 import  App from '../components/App/App'
-import { Login_Form } from '../components/Login/login'
-import { Register_Form } from '../components/Register/register'
+import { Login_Form } from '../components/Login/Login'
+import { Register_Form } from '../components/Register/Register'
 
 export class Routes extends React.Component{
     render(){
