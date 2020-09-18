04617f7e6caea28840c02591b1552622128259d2
Duy <dev.anhduy@gmail.com>
2020-09-18 14:20:27 +0700

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import { Routes } from '../src/routes/index';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './config';
 
 ReactDOM.render(<Routes />,document.getElementById('root'));
 
