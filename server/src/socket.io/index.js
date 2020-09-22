34c78b3f07ce1702770a72571654af060d4a24df
Duy <dev.anhduy@gmail.com>
2020-09-22 18:54:51 +0700

    Update index.js

--- server/src/socket.io/index.js
+++ server/src/socket.io/index.js
@@ -1,6 +1,7 @@
 const encode_jwt_token = require('src/utils/encode_jwt_token');
 const User_Model = require('src/models/user');
 const message_factory = require('src/socket.io/message_factory');
+const request = require('request');
 
 
 module.exports = (server) => {
@@ -17,7 +18,15 @@ module.exports = (server) => {
         users_connected[user._id] = socket.id ; // Storage socket id of user to object users connected ;
 
         socket.on('/start-chat',(receiver) => {
-            message_factory.start_chat({ io, socket,receiver,users_connected,token, })
+            message_factory.start_chat({ io, socket,receiver,users_connected,token,user })
+        })
+        socket.on("disconnect",() => {
+            request({
+                method: 'put',
+                url: `${__host}:${__port}/users/${user._id}`,
+                headers : { authorization : token },
+                json : {list_chats : user.list_chats}
+            })
         })
     })
 }
