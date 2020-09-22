244c4570bd6b6d6250a2ac8d2c455a4f500c54ce
Duy <dev.anhduy@gmail.com>
2020-09-22 18:54:34 +0700

    Update socket_handle_factory.js

--- client/src/components/App/socket_handle_factory.js
+++ client/src/components/App/socket_handle_factory.js
@@ -15,32 +15,41 @@ const socket_handle_factory = {
         }
     },
     receiver_from_server : {
-        message : (socket,user,obj_receivers) => {
+        message : ({socket,user,obj_receivers}) => {
             socket.on('/receiver-message', (msg) => {
-                //Get main message element
-                const main_message = document.getElementById('main-message');
-                //Create div wrap message
-                let wrap_message = document.createElement('div');
-                //Check if sender is user
-                if (msg.sender_id === user._id)
-                  wrap_message.className = 'messages sender';
-                else { 
-                  wrap_message.className = 'messages';
-                  //Create div  element for avatar
-                  let avatar_message = document.createElement('div');
-                  avatar_message.className = "avatar-message";
-                  avatar_message.style.backgroundImage = `url("${obj_receivers[msg.sender_id].avatar}")`;
-                  //Append avatar message in wrap message
-                  wrap_message.appendChild(avatar_message);
-                }
-                //Create element for message
-                let span_message = document.createElement('span');
-                span_message.innerHTML = `${msg.content}`;
-                //Append span message in wrap message
-                wrap_message.appendChild(span_message);
-                //Append wrap message in main message
-                main_message.appendChild(wrap_message);
-                main_message.scrollTop = main_message.scrollHeight;
+                const isChatPage = window.location.href.includes("http://localhost:3000/chat");
+                if (isChatPage) {
+                    //*Update after ...
+                }      
+                    console.log(msg)
+                    const page_url = window.location.pathname;
+                    const receiver_active = page_url.substring(page_url.lastIndexOf("/") + 1);
+                    if(receiver_active === msg.sender_id || receiver_active === msg.receiver_id || user._id === msg.sender_id ){
+                        //Get main message element
+                        const main_message = document.getElementById('main-message');
+                        //Create div wrap message
+                        let wrap_message = document.createElement('div');
+                        //Check if sender is user
+                        if (msg.sender_id === user._id)
+                            wrap_message.className = 'messages sender';
+                        else { 
+                            wrap_message.className = 'messages';
+                            //Create div  element for avatar
+                            let avatar_message = document.createElement('div');
+                            avatar_message.className = "avatar-message";
+                            avatar_message.style.backgroundImage = `url("${obj_receivers[msg.sender_id].avatar}")`;
+                            //Append avatar message in wrap message
+                            wrap_message.appendChild(avatar_message);
+                        }
+                        //Create element for message
+                        let span_message = document.createElement('span');
+                        span_message.innerHTML = `${msg.content}`;
+                        //Append span message in wrap message
+                        wrap_message.appendChild(span_message);
+                        //Append wrap message in main message
+                        main_message.appendChild(wrap_message);
+                        main_message.scrollTop = main_message.scrollHeight;
+                    }
             })
         }
     }
