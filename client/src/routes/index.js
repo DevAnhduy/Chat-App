8be47ead7486fe8fa794b95f62510359fa26f227
DevAnhduy <dev.anhduy@gmail.com>
2020-09-27 01:08:34 +0700

    Update index.js

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,8 +1,9 @@
-import React from 'react'
-import {BrowserRouter as Router,Route,Switch } from 'react-router-dom'
-import  App from '../components/App/App'
-import { Login_Form } from '../components/Login/Login'
-import { Register_Form } from '../components/Register/Register'
+import React from 'react';
+import {BrowserRouter as Router,Route,Switch } from 'react-router-dom';
+import  App from '../components/App/App';
+import HomePage from '../components/Homepage/HomePage';
+import { Login_Form } from '../components/Login/Login';
+import { Register_Form } from '../components/Register/Register';
 
 export class Routes extends React.Component{
     render(){
@@ -12,6 +13,7 @@ export class Routes extends React.Component{
                     <Route exact path="/" component={App} />
                     <Route path="/login" component={Login_Form} />
                     <Route path="/register" component={Register_Form} />
+                    <Route path="/homepage" component={HomePage} />
                     <Route path="/chat/:receiver_type/:receiver_id" component={App} />
                 </Switch>
             </Router>
