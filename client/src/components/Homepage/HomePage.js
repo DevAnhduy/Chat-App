b641b52f9ab6ef71be5c8524926b97faa1b01580
DevAnhduy <dev.anhduy@gmail.com>
2020-09-28 01:19:00 +0700

    Update HomePage.js

--- client/src/components/Homepage/HomePage.js
+++ client/src/components/Homepage/HomePage.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from './Navbar.js';
 import Hero from './Hero.js';
 import Features from './Features.js';
+import AwesomeKeyFeatures from './AwesomeKeyFeatures.js';
+import Newsletter from './Newsletter.js';
+import Footer from './Footer.js';
 import './Homepage.scss';
 
 const HomePage = props => {
+    const [scroll_bot, set_scroll_bot] = useState(0);
+    const window_height = window.innerHeight;
+
+    useEffect(() => {
+        const onScroll = e => {
+            set_scroll_bot(e.target.documentElement.scrollTop + window_height);
+        };
+        window.addEventListener("scroll", onScroll);
+        return () => window.removeEventListener("scroll", onScroll);
+    }, [scroll_bot]);
     return (
-        <div>
+        <div style={{paddingTop:100}}>
             <Navbar />
-            <Hero />
-            <Features />
+            <Hero scroll_bot={scroll_bot} />
+            <Features scroll_bot={scroll_bot} />
+            <AwesomeKeyFeatures />
+            <Newsletter />
+            <Footer />
         </div>
     )
 }
