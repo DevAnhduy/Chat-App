5b923a9a8b784d670af7b110fe8e12f3f3b84c40
DevAnhduy <dev.anhduy@gmail.com>
2020-10-03 23:06:15 +0700

    Update App_v2.js

--- client/src/components/App_v2/App_v2.js
+++ client/src/components/App_v2/App_v2.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Chat_Left_Sidebar from './Chat_Left_Sidebar';
 import Favorite_Left_Sidebar from './Favorite_Left_Sidebar';
 import Friend_Left_Sidebar from './Friend_Left_Sidebar';
@@ -9,6 +9,8 @@ import User_Profile from'./User_Profile';
 import Setting from './Setting';
 import Navigation from './Navigation';
 import './App_v2.scss';
+import { Voice_Call_Request,Voice_Call_Accepted } from './Voice_Call_Modal';
+import { Video_Call_Accepted, Video_Call_Request } from './Video_Call_Modal';
 
 const App_v2 = props => {
     return (
@@ -16,14 +18,18 @@ const App_v2 = props => {
             <div className="layout">
                 <Navigation />
                 <Chat_Left_Sidebar />
-                {/* <Friend_Left_Sidebar />
+                <Friend_Left_Sidebar />
                 <Favorite_Left_Sidebar />
-                <Archived_Left_Sidebar /> */}
+                <Archived_Left_Sidebar />
                 <Chat /> 
             </div>
-            {/* <Notification />
+            <Notification />
             <User_Profile />
-            <Setting /> */}
+            <Setting />
+            <Voice_Call_Request />
+            <Voice_Call_Accepted />
+            <Video_Call_Request />
+            <Video_Call_Accepted />
         </div>
     )
 }
