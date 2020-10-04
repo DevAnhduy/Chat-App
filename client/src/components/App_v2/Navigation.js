16ae961222ca89c90be0d45b6a55ad18b0c05707
DevAnhduy <dev.anhduy@gmail.com>
2020-10-04 22:01:48 +0700

    Update Navigation.js

--- client/src/components/App_v2/Navigation.js
+++ client/src/components/App_v2/Navigation.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import $ from 'jquery';
+import { ReactComponent as Bell_Icon } from '../../assets/svgs/bell.svg'
 const logo = require('../../assets/logo.png');
-const bell_icon = require('../../assets/svgs/bell.svg');
+//const bell_icon = require('../../assets/svgs/bell.svg');
 
 const Navigation = props => {
     return(
@@ -18,8 +19,8 @@ const Navigation = props => {
                         </a>
                         <div className="dropdown-menu dropdown-menu-right">
                             <a href="#" className="dropdown-item" data-left-sidebar="friends">Bắt đầu chat</a>
-                            <a href="#" className="dropdown-item" data-toggle="modal" data-target="#newGroup">Tạo nhóm chat</a>
-                            <a href="#" className="dropdown-item" data-toggle="modal" data-target="#intiveUsers">Mời bạn bè</a>
+                            <a href="#" className="dropdown-item" data-toggle="modal" data-target="#new_room">Tạo nhóm chat</a>
+                            <a className="dropdown-item" data-toggle="modal" data-target="#add_friend">Thêm bạn bè</a>
                         </div>
                     </li>
                     <li onClick={() => { $(".left-sidebar").removeClass("open"); $("#chats").addClass("open") }}>
@@ -47,8 +48,7 @@ const Navigation = props => {
                     </li>
                     <li onClick={() => $("#notifications").addClass("open")}  >
                         <a data-toggle="tooltip" title="Thông báo" data-placement="right">
-                            <svg width={24} height={24} xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" stroke="currentColor" clipRule="evenodd"><path d="M1 20v-2.946c1.993-.656 2.575-2.158 3.668-6.077.897-3.218 1.891-6.784 4.873-8.023-.027-.147-.041-.299-.041-.454 0-1.38 1.12-2.5 2.5-2.5s2.5 1.12 2.5 2.5c0 .156-.014.309-.042.458 2.987 1.244 3.984 4.813 4.884 8.033 1.103 3.95 1.697 5.423 3.658 6.062v2.947h-7c0 2.208-1.792 4-4 4s-4-1.792-4-4h-7zm14 0h-6c0 1.656 1.344 3 3 3s3-1.344 3-3zm-13-1h20v-1.241c-2.062-.918-2.82-3.633-3.621-6.498-1.066-3.814-2.167-7.758-6.379-7.761-4.21 0-5.308 3.937-6.369 7.745-.8 2.872-1.559 5.593-3.631 6.514v1.241zm11.492-16.345l.008-.155c0-.828-.672-1.5-1.5-1.5s-1.5.672-1.5 1.5l.008.152c.455-.099.949-.152 1.487-.152.542 0 1.039.054 1.497.155z" /></svg>
-
+                            <Bell_Icon />
                         </a>
                     </li>
                     <li className="brackets" onClick={() => { $(".left-sidebar").removeClass("open"); $("#archived").addClass("open") }}>
@@ -78,7 +78,7 @@ const Navigation = props => {
                             <a href="#" className="dropdown-item" data-right-sidebar="user-profile">
                                 Thông tin cá nhân
                             </a>
-                            <a href="#" className="dropdown-item" data-toggle="modal" data-target="#settingsModal">
+                            <a href="#" className="dropdown-item" data-toggle="modal" data-target="#setting_modal">
                                 Cài đặt
                             </a>
                             <a href="#" className="dropdown-item d-none d-md-block example-app-tour-start">
